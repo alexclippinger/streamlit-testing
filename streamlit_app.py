8b80985be92d06aa666e796f419618bfa289b451
Alex Clippinger <72150691+alexclippinger@users.noreply.github.com>
2022-06-06 14:01:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,21 @@
 import streamlit as st
 import psycopg2
-import os
-
-st.write("Has environment variables been set:",
-	 os.environ["user"] == st.secrets["user"])
 
 def init_connection():
 	return psychopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from addresses limit 10;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
