796c4df17d5577e6085c8edaac72d46297f8624f
Alex Clippinger <72150691+alexclippinger@users.noreply.github.com>
2022-06-06 13:54:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import psycopg2
 import os
 
-st.write(
-	"Has environment variables been set:",
-	os.environ["ALTOS_DB_USERNAME"] == st.secrets["ALTOS_DB_USERNAME"])
+st.write("Has environment variables been set:",
+	 os.environ["ALTOS_DB_USERNAME"] == st.secrets["ALTOS_DB_USERNAME"])
+
+def init_connection():
+	return psychopg2.connect(**st.secrets["postgres"])
